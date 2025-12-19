119699c053f8a1169a7e9f06e8ed464233122bc7
oman ramalho <omanpple42@Hotmail.com>
2025-12-19 02:43:51 -0300

    fix/comment component on initial page

---
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface LayoutProps {}
+
+export default function Layout({ }: LayoutProps) {
+  return (
+    <div>Layout</div>
+  )
+}
--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -15,7 +15,7 @@ export default function HomeScreen() {
   return (
     <main className="min-h-screen">
       <Header />
-      <HeroSection />
+      {/* <HeroSection />
       <StatsSection />
       <LogosSection />
       <ServicesSelector />
@@ -25,7 +25,7 @@ export default function HomeScreen() {
       <CtaSection />
       <PricingSection />
       <FaqSection />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
